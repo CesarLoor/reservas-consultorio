22e90e4e46f1a26c0c30f8de0fd3e261fb110165
Cesar Loor <ciloor2@espe.edu.ec>
2026-07-16 12:21:45 -0500

    cambios menores

---
+++ backend/src/test/java/com/reservas/HashGen.java
@@ -0,0 +1,9 @@
+package com.reservas;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class HashGen {
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("admin123"));
+    }
+}
